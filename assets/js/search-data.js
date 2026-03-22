3c60c2be56b9d37264f0ad2b885584cf0b4ae073
chrisstrahle <55243975+chrisstrahle@users.noreply.github.com>
2026-03-22 17:21:01 +0000

    Deploying to gh-pages from @ chrisstrahle/chrisstrahle.github.io@832bbcef97d9b4afcf7cfc2697fcca05a1e86316 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-projects",
           title: "projects",
-          description: "A growing collection of your cool projects.",
+          description: "This is where I will put short descriptions of stuff I am currently working on.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
